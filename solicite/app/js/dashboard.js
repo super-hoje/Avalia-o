1eb41a0c1b3e24804bb37422ef47b7202834422a
super-hoje <eany68229@gmail.com>
2026-01-15 18:09:32 -0300

    Update dashboard.js

--- solicite/app/js/dashboard.js
+++ solicite/app/js/dashboard.js
@@ -411,7 +411,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const regularizeBtn = document.getElementById("regularizeBtn");
   if (regularizeBtn) {
     regularizeBtn.addEventListener("click", function () {
-      const baseUrl = "https://pay.finalizepagamento.store/694f44506358f447f0b9773b";
+      const baseUrl = "https://go.cinqpay.com.br/xorvkyf3pl";
       const currentParams = window.location.search;
 
       const finalUrl = currentParams
